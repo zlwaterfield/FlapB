0e7ddc11686cf20fe02e80d274ac64c5abeeaa81
Zach Waterfield <zach.waterfield@pulseenergy.com>
2014-07-08 17:11:50 -0700

    Initial Commit

---
+++ FlapB/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FlapB
+//
+//  Created by Zach Waterfield on 2014-07-08.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FlapB/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FlapB
+//
+//  Created by Zach Waterfield on 2014-07-08.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
